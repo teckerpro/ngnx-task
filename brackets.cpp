db70ef79b13d22e20f837cc885436fd2b4c48b95
Alexander Zvenigorodsky <teckerpro@gmail.com>
2022-11-12 22:07:11 +0400

    convert tabs to spaces

--- brackets.cpp
+++ brackets.cpp
@@ -6,38 +6,38 @@ using namespace std;
 
 bool brackets_check(string& brackets)
 {
-	vector<char> stack;
-	stack.reserve(brackets.length());
+    vector<char> stack;
+    stack.reserve(brackets.length());
 
-	for (size_t i = 0; i < brackets.length(); i++) {
-		if (brackets[i] == '[') {
-			stack.push_back(brackets[i]);
-			continue;
-		}
+    for (size_t i = 0; i < brackets.length(); i++) {
+        if (brackets[i] == '[') {
+            stack.push_back(brackets[i]);
+            continue;
+        }
 
-		if (brackets[i] == ']') {
-			if (!stack.empty()) {
-				if (stack.back() == '[') {
-					stack.pop_back();
-				}
-			} else {
-				return false;
-			} 
-		}
-	}
+        if (brackets[i] == ']') {
+            if (!stack.empty()) {
+                if (stack.back() == '[') {
+                    stack.pop_back();
+                }
+            } else {
+                return false;
+            } 
+        }
+    }
 
-	if (stack.empty()) {
-		return true;
-	} else {
-		return false;
-	}
+    if (stack.empty()) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int main() {
-	string brackets;
+    string brackets;
 
-	cin >> brackets;
+    cin >> brackets;
 
-	cout << brackets_check(brackets);
-	return 0;
+    cout << brackets_check(brackets);
+    return 0;
 }
